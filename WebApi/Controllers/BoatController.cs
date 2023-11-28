92eff86b327778f11113dbff0e7a0227e18b7ca1
bflora03 <flora.bernath@bce.bitclub.hu>
2023-11-28 11:13:38 +0100

    gyak11

---
+++ WebApi/Controllers/BoatController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BoatController : ControllerBase
+    {
+        
+        [HttpGet]
+        [Route("questions/all")]
+        public IActionResult MindegyHogyHivjak()
+        {
+            HajosContext context = new HajosContext();
+            var kérdések = from x in context.Questions select x.Question1;
+
+            return Ok(kérdések);
+        }
+    
+    }
+}
